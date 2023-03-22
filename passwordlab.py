93b1088013b78ea1bd89f4f9eac2ff67160c3caf
Arlen Larry G <larry-gyden@Arlens-MacBook-Air.local>
2023-03-22 09:17:27 -0400

    added encode and main function

--- passwordlab.py
+++ passwordlab.py
@@ -6,3 +6,29 @@ def encode():
         number = int(number) + 3
         encod_password += str(number)
     return orig_password, encod_password
+
+# Create main function
+def main():
+    # Open menu while loop
+    password_encoder = True
+    while password_encoder:
+        # Print menu
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit", end='\n\n')
+
+        # Prompt user menu selection
+        user_select = int(input("Please enter an option: "))
+        if user_select == 1:
+            orig_password, encod_password = encode()
+            print("Your password has been encoded and stored!", end='\n\n')
+        if user_select == 2:
+            print(f"The encoded password is {encod_password}, and the original password is {orig_password}.", end='\n\n')
+        if user_select == 3:
+            exit()
+
+
+if __name__ == "__main__":
+    main()
